aa9f481babeebf1461f654e43915dcd87465aaaf
devoliv <r7mon.oli@icloud.com>
2024-02-22 18:10:58 -0300

    Adicionando funcionalidade de mostrar mensagem

--- cadastro.js
+++ cadastro.js
@@ -4,3 +4,11 @@ function signUp(email){
 
 
 //pegando email e cadastrando na plataforma
+
+
+function mostraMensagem(email){
+    return "Bem vindo ao sistema";
+
+}
+
+//Adicionando função de mensagem
